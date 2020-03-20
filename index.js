a425a8349018accd1bd38ee4c880e926788b35c2
T-O1427 <t.omalley1427@gmail.com>
2020-03-20 21:52:49 +0000

    [Feature] Added functionality to search loaned albums

--- index.js
+++ index.js
@@ -58,6 +58,65 @@ app.get("/user/:id/albums/:albumId", (req, res, next) => {
     res.json(response);
 });
 
+app.post("/user/:id/albums/:albumId/loan", (req, res, next) => {
+    let payload = req.body;
+
+    let rawdata = FileSystem.readFileSync('data.json');
+    let users = JSON.parse(rawdata);
+    let response = {};
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].id === req.params.id) {
+            for (let k = 0; k < users[i].albums.length; k++) {
+                if (users[i].albums[k].id === req.params.albumId) {
+                    if (!Array.isArray(users[i].albums[k].loans)) {
+                        users[i].albums[k].loans = [];
+                    }
+
+                    users[i].albums[k].loans.push({userName: payload.username, date: payload.date});
+                    response = users[i].albums[k];
+                    break;
+                }
+            }
+        }
+    }
+
+    /*Writing the file*/
+    FileSystem.writeFile('data.json', JSON.stringify(users, null, 4), function (err) {
+        if (err) throw err;
+        console.log('Replaced!');
+    });
+
+    /**
+    * you search for the album that you need to loan
+     * you add the loaner information to the album that you found
+     * you save the file with the amended information
+     * then return a response that you amended the album with the loaner information
+    * */
+    res.json(response);
+});
+
+app.get("/user/:id/albums/loans/:username", (req, res, next) => {
+    let rawdata = FileSystem.readFileSync('data.json');
+    let users = JSON.parse(rawdata);
+    let response = [];
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].id === req.params.id) {
+            for (let k = 0; k < users[i].albums.length; k++) {
+                if (!Array.isArray(users[i].albums[k].loans)) {
+                    users[i].albums[k].loans = [];
+                }
+                for (let l = 0; l < users[i].albums[k].loans.length; l++) {
+                    if (users[i].albums[k].loans[l].userName === req.params.username) {
+                        response.push(users[i].albums[k]);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    res.json(response);
+});
+
 app.listen(3000, () => console.log(`Example app listening on port ${3000}!`));
 //export app
 module.exports = app;
