4599a26772c246f83712628e804c2e9bf8bc35c3
T-O1427 <t.omalley1427@gmail.com>
2020-03-20 20:58:40 +0000

    [Feature] Added Endpoints to query various data concepts

--- index.js
+++ index.js
@@ -1,28 +1,63 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
-
-
+const FileSystem = require('fs');
 const app = express();
 
-const ads = [
-    {title: 'Hello, world (again)!'}
-];
+// import morgan package
+const morgan = require('morgan');
 
-app.use(helmet());
+//use it
+app.use(morgan('dev'));
+// Routes which should handle request
 
+// import body-parser
+const bodyParser = require('body-parser');
+// let's use it
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use(cors());
-
-app.use(morgan('combined'));
-
-app.get('/', (req, res) => {
-    res.send(ads);
+app.get("/user/:id", (req, res, next) => {
+    let rawdata = FileSystem.readFileSync('data.json');
+    let users = JSON.parse(rawdata);
+    let response = {};
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].id === req.params.id) {
+            response = users[i];
+            break;
+        }
+    }
+    res.json(response);
 });
 
-app.listen(3001, () => {
-    console.log('listening on port 3001');
+app.get("/user/:id/albums", (req, res, next) => {
+    let rawdata = FileSystem.readFileSync('data.json');
+    let users = JSON.parse(rawdata);
+    let response = {};
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].id === req.params.id) {
+            response = users[i].albums;
+            break;
+        }
+    }
+    res.json(response);
 });
+
+app.get("/user/:id/albums/:albumId", (req, res, next) => {
+    let rawdata = FileSystem.readFileSync('data.json');
+    let users = JSON.parse(rawdata);
+    let response = {};
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].id === req.params.id) {
+            for (let k = 0; k < users[i].albums.length; k++) {
+                if (users[i].albums[k].id === req.params.albumId) {
+                    response = users[i].albums[k];
+                    break;
+                }
+            }
+        }
+    }
+    res.json(response);
+});
+
+app.listen(3000, () => console.log(`Example app listening on port ${3000}!`));
+//export app
+module.exports = app;
