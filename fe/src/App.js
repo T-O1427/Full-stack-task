b93305f6601435f67749dea6596a13660c16173f
Taylor <t.omalley1427@gmail.com>
2020-03-22 15:40:40 +0000

    [FEATURE] Updated frontend

--- fe/src/App.js
+++ fe/src/App.js
@@ -6,16 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
           <h1>Nova Futur React App</h1>
+
       </header>
-        <body className="Body">
-        search bar
-        </body>
-        <body className="Button">
-        Search button
-        </body>
-        <body className="Results">
-        Search results
-        </body>
     </div>
   );
 }
