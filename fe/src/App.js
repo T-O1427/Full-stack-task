b50032b0e9468566bd6ca08bec2de744dd2dcfc3
Taylor O'Malley <t.omalley1427@gmail.com>
2020-04-22 13:32:03 +0100

    [FEATURE] Replaced logos

--- fe/src/App.js
+++ fe/src/App.js
@@ -8,9 +8,7 @@ function App() {
     <div className="App container-fluid">
           <div className={'row'}>
                   <header className="App-header">
-                      <img
-                          src="https://uploads-ssl.webflow.com/5d8cbad86fd5bf258c87353e/5d8df17dc33e99853e136009_novafutur_novafutur_Horizontal_noTag_BW.svg"
-                          alt="" className="welcomelogo"/>
+                      
                   </header>
           </div>
 
